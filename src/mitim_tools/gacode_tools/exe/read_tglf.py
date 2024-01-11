b9e8c93b2f268fb877622f4eeda98130ead946fb
Pablo RF <pablorf@mit.edu>
2024-01-11 11:19:07 -0500

    Allow different suffixes per tglf read

--- src/mitim_tools/gacode_tools/exe/read_tglf.py
+++ src/mitim_tools/gacode_tools/exe/read_tglf.py
@@ -1,7 +1,7 @@
 """
 This example reads TGLF from an already existing folder (no normalizations if no input_gacode file provided)
 
-	read_tglf.py run0/ [--suffix _0.55] [--gacode input.gacode]
+	read_tglf.py run0/ [--suffixes _0.55] [--gacode input.gacode]
 
 """
 
@@ -11,18 +11,25 @@ from mitim_tools.gacode_tools import TGLFtools
 
 parser = argparse.ArgumentParser()
 parser.add_argument("folders", type=str,nargs='*')
-parser.add_argument("--suffix", required=False, type=str, default="")
+parser.add_argument("--suffixes", required=False, type=str, nargs='*',default=None)
 parser.add_argument("--gacode", required=False, type=str, default=None)
 
 args = parser.parse_args()
 
 folders = [IOtools.expandPath(folder) for folder in args.folders]
 input_gacode = IOtools.expandPath(args.gacode) if args.gacode is not None else None
-suffix = args.suffix
+suffixes = args.suffixes
+
+if suffixes is None:
+	suffixes = ["" for _ in range(len(folders))]
+
+for i in range(len(suffixes)):
+	if suffixes[i] == "_":
+		suffixes[i] = ""
 
 tglf = TGLFtools.TGLF()
-tglf.prep_from_tglf(folders[0], f"{folders[0]}/input.tglf{suffix}", input_gacode=input_gacode)
+tglf.prep_from_tglf(folders[0], f"{folders[0]}/input.tglf{suffixes[0]}", input_gacode=input_gacode)
 for i,folder in enumerate(folders):
-	tglf.read(folder=f"{folder}/", suffix=suffix, label=f"run{i}")
+	tglf.read(folder=f"{folder}/", suffix=suffixes[i], label=f"run{i}")
 
 tglf.plotRun(labels=[f"run{i}" for i in range(len(folders))])
