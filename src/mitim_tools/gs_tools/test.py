62c3bb9a59f9e3f1be38ebf4bcc7d5c856ceaaa8
Pablo RF <pablorf@mit.edu>
2024-05-29 17:29:01 -0400

    Removed test.py

--- src/mitim_tools/gs_tools/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mitim_tools.misc_tools import GRAPHICStools, MATHtools
-from IPython import embed
-from mitim_tools.gs_tools import GEQtools
-
-g = GEQtools.MITIMgeqdsk('/Users/hallj/Documents/Files/Research/ARC Modeling/ASTRA-POPCON matching/astra.geqdsk')
-Rb, Yb = g.get_MXH_coeff()
