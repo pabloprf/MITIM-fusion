6a8a309bb566c879e8eead27e0416c3d94c17372
Pablo RF <pablorf@mit.edu>
2024-08-14 18:25:42 -0400

    misc

--- src/mitim_tools/opt_tools/STRATEGYtools.py
+++ src/mitim_tools/opt_tools/STRATEGYtools.py
@@ -901,7 +901,7 @@ class PRF_BO:
             try:
                 pickle_dill.dump(copyClass, handle)
             except:
-                print(f"\t* Problem saving {name}, trying without the optimization_object, but that will lead to limiting applications", typeMsg="w")
+                print(f"\t* Problem saving {name}, trying without the optimization_object, but that will lead to limiting applications. I recommend you populate self.optimization_object.doNotSaveVariables with the variables you think cannot be pickled", typeMsg="w")
                 del copyClass.optimization_object
                 pickle_dill.dump(copyClass, handle)
 
