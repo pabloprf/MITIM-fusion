290a6ff6461169ecb73d40d0b4af50b4c9cdf6f8
josephb-hall <joehallsv@gmail.com>
2024-08-15 11:18:53 -0400

    basic working example

--- src/mitim_tools/surrogate_tools/NNtools.py
+++ src/mitim_tools/surrogate_tools/NNtools.py
@@ -1,4 +1,7 @@
 from mitim_tools.misc_tools import IOtools
+import numpy as np
+import os
+
 # ---------------------------------------------------------------------------------------------
 # Main class for Neural Network tools
 # ---------------------------------------------------------------------------------------------
@@ -13,15 +16,28 @@ class mitim_nn:
             self.load = self._load_tf
             self.__call__ = self._evaluate_tf
 
-    def _load_tf(self, model_path):
+    def _load_tf(self, model_path, norm=None):
 
-        #from tensorflow.keras.models import load_model
-        #return load_model(model_path)
+        from tensorflow.keras.models import load_model
+
+        self.model = load_model(model_path)
+        self.norm = None
+
+        if norm is not None:
+            self.norm = np.loadtxt('/Users/hallj/MITIM-fusion/src/mitim_tools/surrogate_tools/EPED-NN-NORMALIZATION.txt')
+            print(f'\t- Normalization file from {IOtools.clipstr(norm,30)} loaded')
+            print("Norm:", self.norm)
+        
         print(f'\t- Weights file from {IOtools.clipstr(model_path,30)} loaded')
         
     def _evaluate_tf(self, inputs):
-
+        
         print('testing evaluation with ', inputs)
+
+        if self.norm is not None:
+            return self.model.predict(inputs)[0]*self.norm
+        else:
+            return self.model.predict(inputs)[0]
 
 
 # ---------------------------------------------------------------------------------------------
@@ -33,9 +49,24 @@ class eped_nn(mitim_nn):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def __call__(self, R, a, kappa):
+    def __call__(self, 
+                 R,             #m
+                 a,             #m
+                 Ip,            # MA
+                 Bt,            # T
+                 kappa995, 
+                 delta995, 
+                 neped,         #10e19m^-3
+                 betan, 
+                 zeff, 
+                 nesep_ratio=0.3, 
+                 tesep=75    #eV
+                 ):
 
-        inputs = [R, a, kappa]
+        nesep = neped*nesep_ratio
+        inputs = np.array([Ip, Bt, R, a, kappa995, delta995, neped, betan, zeff, tesep, nesep])
+
+        self._evaluate_tf(inputs)
 
         return self.__call__(inputs)
 
@@ -45,6 +76,8 @@ class eped_nn(mitim_nn):
 if __name__ == '__main__':
 
     nn = eped_nn(type='tf')
-    nn.load('model.h5')
-    p_top = nn(1.67, 0.6, 1.75)
-
+    nn.load('/Users/hallj/MITIM-fusion/src/mitim_tools/surrogate_tools/EPED-NN-MODEL-ARC.h5', 
+            norm="/Users/hallj/MITIM-fusion/src/mitim_tools/surrogate_tools/EPED-NN-NORMALIZATION.txt"
+            )
+    ptop, wtop = nn(4.25, 1.17, 10.95, 10.8, 1.68, 0.516, 19.27, 1.9, 1.5)
+    print(ptop, wtop)
