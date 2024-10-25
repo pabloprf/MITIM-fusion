c944a63c663aa7b7b79c73831c20269917b63cd4
AudreySaltzman <audreysa@mit.edu>
2024-10-25 15:20:04 -0400

    __mitimroot__ to path object

--- src/mitim_tools/__init__.py
+++ src/mitim_tools/__init__.py
@@ -1,4 +1,4 @@
 from mitim_tools.misc_tools.CONFIGread import config_manager
 from pathlib import Path
 __version__ = "3.0.0"
-__mitimroot__ = Path(__file__).resolve().parents[2].as_posix()
+__mitimroot__ = Path(__file__).resolve().parents[2]
