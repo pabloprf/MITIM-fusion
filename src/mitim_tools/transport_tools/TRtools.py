24fa91ffd80254052b093444547d07b6bd150a71
Pablo RF <pablorf@mit.edu>
2024-11-22 16:30:51 -0500

    Creation of transport parent class

---
+++ src/mitim_tools/transport_tools/TRtools.py
@@ -0,0 +1,28 @@
+''' 
+General transport model class
+'''
+
+class transport_model():
+    def __init__(self):
+        '''
+        Initialize the transport model 
+        '''
+        pass
+
+    def prep(self):
+        '''
+        Prepare the transport model
+        '''
+        pass
+
+    def run(self):
+        '''
+        Run the transport model
+        '''
+        pass
+
+    def plot(self):
+        '''
+        Plot the transport model
+        '''
+        pass
