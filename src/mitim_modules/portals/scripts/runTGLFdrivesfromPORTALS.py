2be47223b1568086ef7b37bd8b9acb539643abb1
Pablo RF <pablorf@mit.edu>
2025-01-23 20:58:45 -0500

    Added number of poitns as parameter for scans portals

--- src/mitim_modules/portals/scripts/runTGLFdrivesfromPORTALS.py
+++ src/mitim_modules/portals/scripts/runTGLFdrivesfromPORTALS.py
@@ -6,7 +6,7 @@ from mitim_modules.portals.utils import PORTALSanalysis
 This script is useful to understand why surrogates may fail at reproducing TGLF fluxes.
 You can select the iteration to use as base case to see how TGLF behaves (if it has discontinuities)
 	e.g.
-		runTGLFdrivesfrommitim.py --folder run11/ --ev 5 --pos 0 2 --var 0.05  --wf 0.2 1.0
+		runTGLFdrivesfrommitim.py --folder run11/ --ev 5 --pos 0 2 --var 0.05  --wf 0.2 1.0 --num 5
 
 Notes:
 	- wf runs scan with waveform too (slightly more expensive, as it will require 1 extra sim per run, but cheaper)
@@ -19,10 +19,10 @@ parser.add_argument("--folder", required=True, type=str)
 parser.add_argument("--ev", type=int, required=False, default=None)
 parser.add_argument("--pos", type=int, required=False, default=[0], nargs="*")
 parser.add_argument("--wf", type=float, required=False, default=None, nargs="*")
-parser.add_argument(
-    "--var", type=float, required=False, default=0.01
-)  # Variation in inputs (1% default)
+parser.add_argument("--var", type=float, required=False, default=0.01)  # Variation in inputs (1% default)
+parser.add_argument("--num",type=int, required=False, default=5)
 parser.add_argument("-r", required=False, default=False, action="store_true")
+
 
 args = parser.parse_args()
 folder = IOtools.expandPath(args.folder)
@@ -30,6 +30,7 @@ ev = args.ev
 pos = args.pos
 wf = args.wf
 var = args.var
+num = args.num
 cold_start = args.r
 
 # --- Workflow
@@ -39,7 +40,7 @@ tglf, TGLFsettings, extraOptions = portals.extractTGLF(positions=pos, evaluation
 
 tglf.runScanTurbulenceDrives(
     subFolderTGLF="turb",
-    resolutionPoints=5,
+    resolutionPoints=num,
     variation=var,
     variablesDrives=["RLTS_1", "RLTS_2", "RLNS_1", "XNUE", "TAUS_2", "BETAE"],
     TGLFsettings=TGLFsettings,
