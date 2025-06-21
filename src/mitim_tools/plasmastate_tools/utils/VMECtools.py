e650b4aad3b0bafc42eacf2a868c727a85f03094
Pablo RF <pablorf@mit.edu>
2025-06-21 16:16:27 +0200

    Started VMEC tools

---
+++ src/mitim_tools/plasmastate_tools/utils/VMECtools.py
@@ -0,0 +1,26 @@
+
+from mitim_tools.plasmastate_tools.MITIMstate import mitim_state
+
+class vmec_state(mitim_state):
+    '''
+    Class to read and manipulate VMEC files
+    '''
+
+    # ------------------------------------------------------------------
+    # Reading and interpreting
+    # ------------------------------------------------------------------
+
+    def __init__(self, file, calculateDerived=True, mi_ref=None):
+
+        super().__init__(type_file='vmec')
+
+        self.file = file
+
+        self._read_vmec()
+
+        if self.file is not None:
+            # Derive (Depending on resolution, derived can be expensive, so I mmay not do it every time)
+            self.derive_quantities(mi_ref=mi_ref, calculateDerived=calculateDerived)
+
+    def _read_vmec(self):
+        pass
