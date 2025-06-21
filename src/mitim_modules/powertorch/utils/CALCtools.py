611ab007f2ea9b2b7b611cfdd19a38f517469f04
Pablo RF <pablorf@mit.edu>
2025-06-21 20:45:06 +0200

    misc

--- src/mitim_modules/powertorch/utils/CALCtools.py
+++ src/mitim_modules/powertorch/utils/CALCtools.py
@@ -129,7 +129,7 @@ https://github.com/aliutkus/torchinterp1d
 ----------------------------------------------------------------------------------------------------------------
 """
 
-class Interp1d(torch.autograd.Function):
+class Interp1d_torch(torch.autograd.Function):
     def __call__(self, x, y, xnew, out=None):
         return self.forward(x, y, xnew, out)
 
