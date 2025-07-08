8593084630f3d92d05b11fd4b91a002427e7fd01
Pablo RF <pablorf@mit.edu>
2025-07-08 10:24:51 -0400

    Possibility for more than one tmin in CGYRO reader

--- src/mitim_tools/gacode_tools/scripts/read_cgyro.py
+++ src/mitim_tools/gacode_tools/scripts/read_cgyro.py
@@ -12,20 +12,23 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("folders", type=str, nargs="*")
     parser.add_argument("--linear", action="store_true", help="linear run")
-    parser.add_argument("--tmin", type=float, default=0.0, help="Minimum time to calculate mean and std")
+    parser.add_argument("--tmin", type=float, nargs="*", default=None, help="Minimum time to calculate mean and std")
     args = parser.parse_args()
 
     folders = args.folders
     linear = args.linear
     tmin = args.tmin
-
+    
+    if tmin is None:
+        tmin = [0.0] * len(folders)
+    
     # Read
     c = CGYROtools.CGYRO()
 
     labels = []
     for i, folder in enumerate(folders):
         labels.append(f"case {i + 1}")
-        c.read(label=labels[-1], folder=folder, tmin=tmin)
+        c.read(label=labels[-1], folder=folder, tmin=tmin[i])
 
     if linear:
         # Plot linear spectrum
