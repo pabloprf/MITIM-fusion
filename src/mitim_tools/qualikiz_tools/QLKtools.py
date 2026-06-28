5ee620ffe1ab00d7259e4e403c36d8480eca8179
aaronkho <aaronkho@mit.edu>
2026-06-28 12:10:37 -0400

    Adjusted fast ions to be included in dilution and quasineutrality

--- src/mitim_tools/qualikiz_tools/QLKtools.py
+++ src/mitim_tools/qualikiz_tools/QLKtools.py
@@ -431,7 +431,7 @@ def _build_plan_from_gacode(profiles, rhos, code_settings=None):
     ion_types = []
     for i in range(n_ions):
         is_fast = p.Species[i].get("S", "therm") == "fast"
-        ion_types.append(3 if is_fast else 1)
+        ion_types.append(4 if is_fast else 1)
         ion_scan[f"Ti{i}"] = interp_vec(p.profiles["ti(keV)"][:, i])
         ion_scan[f"ni{i}"] = interp_vec(p.derived["fi"][:, i])
         ion_scan[f"Ati{i}"] = interp_vec(p.derived["aLTi"][:, i]) * Ro_over_a
@@ -465,9 +465,9 @@ def _build_plan_from_gacode(profiles, rhos, code_settings=None):
         for i in range(n_ions)
     ])
 
-    thermal_indices = [i for i in range(n_ions) if ion_types[i] != 3]
+    thermal_indices = [i for i in range(n_ions) if p.Species[i].get("S", "therm") != "fast"]
     #qn_ion_index = thermal_indices[-1] if thermal_indices else n_ions - 1
-    z_indices = [p.Species[i]["Z"] for i in range(n_ions) if ion_types[i] != 3]
+    z_indices = [p.Species[i]["Z"] for i in range(n_ions) if ion_types[i] not in [3, 4]]
     qn_ion_index = z_indices.index(max(z_indices))
 
     meta_kwargs = QLKdefaults.addQLKcontrol(code_settings)
